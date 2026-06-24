9ec29f19dfbee255e6fcd03af1a2aa53a72ff540
ChristopherDevv <christopher.lopez.dev@gmail.com>
2026-06-24 16:17:05 -0400

    Levantar index

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 // MIDDLEWARES
@@ -6,7 +7,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ARCHIVOS ESTATICOS
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
+
+// RUTA PRINCIPAL
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/index.html"));
+});
 
 module.exports = app;
-
